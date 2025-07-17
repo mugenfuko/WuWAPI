6712092e8faeb6fb21f923dd20336a083697900b
mugenfuko <sebastiaan.hopmans@gmail.com>
2025-07-17 17:15:12 +0200

    Introduced PATCH request function, error handling for HTTP requests, characters are retrievable by name entry

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, exc
 from sqlalchemy.orm import Mapped, mapped_column
 from marshmallow import Schema, ValidationError, fields
 
@@ -17,8 +17,8 @@ class CharacterModel(db.Model):
     element: Mapped[str] = mapped_column(nullable=False)
     weapon: Mapped[str] = mapped_column(nullable=False)
 
-    """def __repr__(self):
-        return f"Character(name= {self.name}"""
+    #def __repr__(self):
+    #    return f"Character(name= {self.name}
 
 class CharacterSchema(Schema):
     id = fields.Int()
@@ -47,11 +47,27 @@ class Character(Resource):
             element = char_data["element"],
             weapon = char_data["weapon"]
         )
-        db.session.add(character)
-        db.session.commit()
+        try:
+            db.session.add(character)
+            db.session.commit()
+        except exc.IntegrityError:
+            db.session.rollback()
         return char_data, 201
 
     def get(self, char_id):
+        try:
+            char_id = int(char_id)
+        except ValueError:
+            try:
+                character = schema.dump(
+                    db.session.execute(
+                        db.select(CharacterModel).where(CharacterModel.name == char_id.lower().capitalize())
+                    ).scalars().one()
+                )
+            except exc.NoResultFound:
+                return {}, 404
+            return character, 200
+
         character = schema.dump(db.session.get(CharacterModel, char_id))
         if not character:
             return {}, 404
@@ -60,21 +76,32 @@ class Character(Resource):
     def put(self, char_id):
         char_data = self.serialize_request(request)
         character = db.session.get(CharacterModel, char_id)
+        if not character:
+            return {}, 404
         character.name = char_data["name"]
         character.rarity = char_data["rarity"]
         character.element = char_data["element"]
         character.weapon = char_data["weapon"]
         db.session.commit()
+        return {}, 200
+
+    def patch(self, char_id):
+        return self.put(char_id)
 
     def delete(self, char_id):
-        db.session.delete(db.session.get(CharacterModel, char_id))
+        character = db.session.get(CharacterModel, char_id)
+        if not character:
+            return {}, 404
+        db.session.delete(character)
         db.session.commit()
         return {}, 204
 
 class CharacterList(Resource):
     def get(self):
         character_list = {}
-        all_characters = db.session.execute(db.select(CharacterModel).order_by(CharacterModel.id)).scalars().all()
+        all_characters = db.session.execute(
+            db.select(CharacterModel).order_by(CharacterModel.id)
+        ).scalars().all()
         for char in all_characters:
             char = schema.dump(char)
             char_id = char["id"]
@@ -88,15 +115,15 @@ api.add_resource(Character, "/characters/<char_id>")
 with app.app_context():
     db.create_all()
 
-characters_dict_example = {
-    "1": {
+characters_arr_example = [
+    {
         "id": 1,
         "name": "Cartethyia",
         "rarity": 5,
         "element": "aero",
         "weapon": "sword"
-        }
-}
+    }
+]
 
 if __name__ == "__main__":
     app.run(debug=True)
